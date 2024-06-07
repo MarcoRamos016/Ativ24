86b21e3602d4bd133bd067203a0922b8e705cc86
MarcoRamos016 <160192932+MarcoRamos016@users.noreply.github.com>
2024-06-06 22:10:36 -0300

    v1

---
+++ Ativ24/Ativ24/Index/script.js
@@ -0,0 +1,45 @@
+document.getElementById("contform").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+
+    // Obtém os valores do formulário
+    var usuario= document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+    
+
+    // Cria um objeto para representar o produto
+    var produto = {
+        usuario: usuario,
+        senha: senha
+
+    };
+
+    // Verifica se a sessionStorage é suportada pelo navegador
+    if (typeof(Storage) !== "undefined") {
+        // Verifica se já existe algum dado na sessionStorage
+        if (sessionStorage.getItem("produtos")) {
+            // Se existir, obtém os dados atuais e converte para um array JavaScript
+            var produtosArray = JSON.parse(sessionStorage.getItem("produtos"));
+            // Adiciona o novo produto ao array
+            produtosArray.push(produto);
+            // Salva o array atualizado na sessionStorage
+            sessionStorage.setItem("produtos", JSON.stringify(produtosArray));
+        } else {
+            // Se não existir, cria um novo array com o produto e salva na sessionStorage
+            var novoArray = [produto];
+            sessionStorage.setItem("produtos", JSON.stringify(novoArray));
+        }
+
+        // Limpa os campos do formulário após o cadastro
+        document.getElementById("contform").reset();
+
+        // Mensagem de sucesso
+        alert("Produto cadastrado com sucesso!");
+
+        location.reload();
+    } else {
+        // Se o navegador não suportar sessionStorage, exibe uma mensagem de erro
+        alert("Seu navegador não suporta a sessionStorage. Não é possível salvar os dados.");
+    }
+
+    
+});
